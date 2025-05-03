a6f635acb9d2778fcd94b996f0618ef0def5ce50
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-03 15:59:55 +0530

    Update DepartmentController.cs

--- Backend/Controllers/DepartmentController.cs
+++ Backend/Controllers/DepartmentController.cs
@@ -1,8 +1,14 @@
+using Backend.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers;
 
 public class DepartmentController: ControllerBase
 {
+    private readonly IDepartmentService _departmentService;
+    public DepartmentController(IDepartmentService departmentService)
+    {
+        _departmentService = departmentService;
+    }
     
 }
