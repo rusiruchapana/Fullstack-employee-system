a7cb4967848f352a73e1bda7c4c2f6f64c5fb8f2
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-03 15:15:47 +0530

    Create EmployeeResponseDto.cs

---
+++ Backend/Dtos/Response/EmployeeResponseDto.cs
@@ -0,0 +1,16 @@
+using Backend.Entities;
+
+namespace Backend.Dtos.Response;
+
+public class EmployeeResponseDto
+{
+    public int EmployeeId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public DateTime HireDate { get; set; }
+    public string Position { get; set; }
+    public decimal Salary { get; set; }
+    public Department Department { get; set; }
+}
