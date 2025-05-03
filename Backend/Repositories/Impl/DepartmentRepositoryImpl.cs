8ef132125336e3ed0d02496022e9cf735560c4c5
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-03 16:00:02 +0530

    Update DepartmentRepositoryImpl.cs

--- Backend/Repositories/Impl/DepartmentRepositoryImpl.cs
+++ Backend/Repositories/Impl/DepartmentRepositoryImpl.cs
@@ -1,6 +1,12 @@
+using Backend.Data;
+
 namespace Backend.Repositories.Impl;
 
 public class DepartmentRepositoryImpl: IDepartmentRepository
 {
-    
+    private readonly ApplicationDbContext _dbContext;
+    public DepartmentRepositoryImpl(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 }
