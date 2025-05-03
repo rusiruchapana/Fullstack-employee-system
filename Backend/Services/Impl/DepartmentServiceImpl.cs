5980397ad0b11d13f0842d9b544997d69f90d5f0
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-03 16:00:00 +0530

    Update DepartmentServiceImpl.cs

--- Backend/Services/Impl/DepartmentServiceImpl.cs
+++ Backend/Services/Impl/DepartmentServiceImpl.cs
@@ -1,6 +1,12 @@
+using Backend.Repositories;
+
 namespace Backend.Services.Impl;
 
 public class DepartmentServiceImpl: IDepartmentService
 {
-    
+    private readonly IDepartmentRepository _departmentRepository;
+    public DepartmentServiceImpl(IDepartmentRepository departmentRepository)
+    {
+        _departmentRepository = departmentRepository;
+    }
 }
