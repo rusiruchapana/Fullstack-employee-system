137b985c12abc82ca2c02045a995bfb664cafbc5
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-04 22:15:29 +0530

    Make routes.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { DepartmentListComponent } from './components/department-list/department-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'employees', component: EmployeeListComponent},
+    {path:'departments', component:DepartmentListComponent},
+    {path:'**', redirectTo: ''} 
+];
