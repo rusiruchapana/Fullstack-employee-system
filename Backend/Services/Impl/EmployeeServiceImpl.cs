6b71399d51b1930d387265bf4ad986b356ec60da
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-04 10:29:10 +0530

    Add dependency injection.

--- Backend/Services/Impl/EmployeeServiceImpl.cs
+++ Backend/Services/Impl/EmployeeServiceImpl.cs
@@ -1,6 +1,13 @@
+using Backend.Repositories;
+
 namespace Backend.Services.Impl;
 
 public class EmployeeServiceImpl: IEmployeeService
 {
-    
+    private readonly IEmployeeRepository _employeeRepository;
+
+    public EmployeeServiceImpl(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
 }
