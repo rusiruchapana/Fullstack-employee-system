f811e4c540a9bf684d05756993b7c67968d83be4
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-04 10:29:14 +0530

    Add dependency injection.

--- Backend/Repositories/Impl/EmployeeRepositoryImpl.cs
+++ Backend/Repositories/Impl/EmployeeRepositoryImpl.cs
@@ -1,6 +1,13 @@
+using Backend.Data;
+
 namespace Backend.Repositories.Impl;
 
 public class EmployeeRepositoryImpl: IEmployeeRepository
 {
-    
+    private readonly ApplicationDbContext _dbContext;
+
+    public EmployeeRepositoryImpl(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 }
