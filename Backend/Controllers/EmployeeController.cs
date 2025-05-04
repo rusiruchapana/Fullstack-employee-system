0665efb8f47e63dcb3c114d2dc63269ab366e834
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-04 10:28:59 +0530

    Add dependency injection.

--- Backend/Controllers/EmployeeController.cs
+++ Backend/Controllers/EmployeeController.cs
@@ -1,6 +1,14 @@
+using Backend.Services;
+
 namespace Backend.Controllers;
 
 public class EmployeeController
 {
-    
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeeController(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
 }
