f13e421ba39cef9b1186eac6c42c9137822652c0
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2025-05-04 16:35:54 +0530

    ######Complete the backend.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,7 +6,6 @@ using Backend.Services;
 using Backend.Services.Impl;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
@@ -15,8 +14,6 @@ builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
-
-
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -28,8 +25,6 @@ builder.Services.AddScoped<IDepartmentService , DepartmentServiceImpl>();
 builder.Services.AddScoped<IDepartmentRepository , DepartmentRepositoryImpl>();
 builder.Services.AddScoped<IEmployeeService , EmployeeServiceImpl>();
 builder.Services.AddScoped<IEmployeeRepository , EmployeeRepositoryImpl>();
-
-
 
 var app = builder.Build();
 
